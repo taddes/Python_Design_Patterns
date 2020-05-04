7835dd5cdc4a65ea85ca23fa73b45d9e63fc0759
Taddes Korris <taddes.korris@gmail.com>
2020-05-04 15:37:26 -0400

    solid Journal class example

--- 1.SOLID Design Principles/single_responsibility.py
+++ 1.SOLID Design Principles/single_responsibility.py
@@ -6,13 +6,23 @@
 """
 
 class Journal:
+    """ Journal class simply allows you to add, remove and read entries"""
     def __init__(self):
         self.entries = []
         self.count = 0
 
-        def add_entry(self, text):
-            self.count += 1
-            self.entries.append(f'{self.count}: {text}')
+    def add_entry(self, text):
+        self.count += 1
+        self.entries.append(f'{self.count}: {text}')
 
-        def remove_entry(self, pos):
-            del self.entries[pos]
+    def remove_entry(self, pos):
+        del self.entries[pos]
+
+    def __str__(self):
+        return '\n'.join(self.entries)
+
+j = Journal()
+j.add_entry('I walked outside.')
+j.add_entry('I love my cat.')
+
+print(f'Journal Entries:\n{j}')
