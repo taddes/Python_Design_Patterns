acb0082583a5fe9ce08cc52bbeec83a0c3d740cc
Taddes Korris <taddes.korris@gmail.com>
2020-05-04 15:55:10 -0400

    antipattern example. Open Closed setup

--- 1.SOLID Design Principles/single_responsibility.py
+++ 1.SOLID Design Principles/single_responsibility.py
@@ -4,6 +4,9 @@
     Single Responsibility Principle - Seperation of Concerns
     A class should have its main responsibilities and nothing more.
 """
+import os
+import sys
+
 
 class Journal:
     """ Journal class simply allows you to add, remove and read entries"""
@@ -21,8 +24,46 @@ class Journal:
     def __str__(self):
         return '\n'.join(self.entries)
 
+    # Not recommended changes - example of bad practice
+    """
+        Secondary responsibility of persistence outside of scope of class.
+        This approach entails having to change every method that may be repeated
+        in another place.
+    """
+    def save(self, filename):
+        file = open(filename, 'w')
+        file.write(str(self))
+        file.close()
+
+    def load(self, filename):
+        pass
+
+    def load_from_web(self, uri):
+        pass
+
+class PersistenceManager:
+    """Enforce sep of concerns. Better implemantation than above"""
+    @staticmethod
+    def save_to_file(journal, filename):
+        file = open(filename, 'w')
+        file.write(str(journal))
+        file.close()
+
+
+
 j = Journal()
 j.add_entry('I walked outside.')
 j.add_entry('I love my cat.')
 
 print(f'Journal Entries:\n{j}')
+
+filepath = os.getcwd() 
+
+PersistenceManager.save_to_file(j, filepath+'\myjournal.txt')
+
+with open(filepath + '\myjournal.txt', 'r') as fh:
+    print(fh.read())
+
+"""
+    Avoid GOD Objects, that contain every possible function. Antipattern. Seperate Concerns.
+"""
